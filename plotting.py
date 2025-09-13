2530684691425055eb15daa84ddd8cfb43232296
amirhosenjafarzade2-netizen <amirhosenjafarzade2@gmail.com>
2025-09-13 17:22:03 +0300

    Create plotting.py

---
+++ plotting.py
@@ -0,0 +1,40 @@
+# plotting.py - Functions for generating plots
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
+
+def plot_fit(x, y, coeffs, method, degree=3):
+    """
+    Plot data points and fitted curve for the given method.
+    """
+    fig, ax = plt.subplots()
+    ax.scatter(x, y, color='blue', label='Data Points', s=50)
+
+    # Generate smooth x values for plotting the curve
+    x_smooth = np.linspace(min(x), max(x), 200)
+
+    if method == "Polynomial":
+        poly = np.poly1d(coeffs)
+        y_smooth = poly(x_smooth)
+        ax.plot(x_smooth, y_smooth, color='red', label=f'Polynomial (deg {degree})')
+    elif method == "Exponential":
+        y_smooth = coeffs[0] * np.exp(coeffs[1] * x_smooth) + coeffs[2]
+        ax.plot(x_smooth, y_smooth, color='red', label='Exponential')
+    elif method == "Logarithmic":
+        y_smooth = coeffs[0] * np.log(x_smooth + 1e-10) + coeffs[1]
+        ax.plot(x_smooth, y_smooth, color='red', label='Logarithmic')
+    elif method == "Compound Poly+Log":
+        y_smooth = coeffs[0] * x_smooth**2 + coeffs[1] * np.log(x_smooth + 1e-10) + coeffs[2]
+        ax.plot(x_smooth, y_smooth, color='red', label='Poly+Log')
+    elif method == "Spline":
+        n_coeffs = len(coeffs) // 2  # Approx split between coeffs and knots
+        spline = UnivariateSpline(x, y, k=degree, s=0)
+        y_smooth = spline(x_smooth)
+        ax.plot(x_smooth, y_smooth, color='red', label='Cubic Spline')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.legend()
+    ax.grid(True)
+    return fig
