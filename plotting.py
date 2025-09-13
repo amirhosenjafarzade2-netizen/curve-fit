800dcb8dcd1999e96fc1ed67034bde18dee1307e
amirhosenjafarzade2-netizen <amirhosenjafarzade2@gmail.com>
2025-09-13 17:35:23 +0300

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -15,7 +15,7 @@ def plot_fit(x, y, coeffs, method, params):
         y_smooth = poly(x_smooth)
         ax.plot(x_smooth, y_smooth, color='red', label=f'Polynomial (deg {params.get("degree", "?")})')
     elif method == "Exponential":
-        y_smooth = coeffs[0] * np.exp(coeffs[1] * x_smooth) + coeffs[2]
+        y_smooth = coeffs[0] * np.exp(coffs[1] * x_smooth) + coeffs[2]
         ax.plot(x_smooth, y_smooth, color='red', label='Exponential')
     elif method == "Logarithmic":
         y_smooth = coeffs[0] * np.log(x_smooth + 1e-10) + coeffs[1]
@@ -25,9 +25,13 @@ def plot_fit(x, y, coeffs, method, params):
         ax.plot(x_smooth, y_smooth, color='red', label='Poly+Log')
     elif method == "Spline":
         degree = params.get('degree', 3)
-        spline = UnivariateSpline(x, y, k=degree, s=0)
-        y_smooth = spline(x_smooth)
-        ax.plot(x_smooth, y_smooth, color='red', label=f'Spline (deg {degree})')
+        try:
+            spline = UnivariateSpline(x, y, k=degree, s=0)
+            y_smooth = spline(x_smooth)
+            ax.plot(x_smooth, y_smooth, color='red', label=f'Spline (deg {degree})')
+        except ValueError:
+            ax.text(0.5, 0.5, 'Spline plot failed: x must be strictly increasing', 
+                    ha='center', va='center', transform=ax.transAxes, color='red')
 
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
